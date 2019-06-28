aa20d97d07844d083b27673cea5c483246caddf0
Eoghan-K <eoghankenny95@gmail.com>
2019-06-28 16:21:00 +0100

    aing mongoose

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID : '880000613058-03hsojbippu122jjn0p570fro9sh51jq.apps.googleusercontent.com',
-    googleClientSecret : 'Wu9ATDdwhkZVAMLs62voWsjE'
+    googleClientSecret : 'Wu9ATDdwhkZVAMLs62voWsjE',
+    mongoURI : 'mongodb://eoghan:Password1@ds343887.mlab.com:43887/email-dev'
 };
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1025;
 app.listen(PORT);
