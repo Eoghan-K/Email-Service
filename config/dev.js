54f47e9f18007a04863584969fa3ff639bbca336
Eoghan-K <eoghankenny95@gmail.com>
2019-06-30 17:06:15 +0100

    finnished auth flow

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID : '880000613058-03hsojbippu122jjn0p570fro9sh51jq.apps.googleusercontent.com',
-    googleClientSecret : 'Wu9ATDdwhkZVAMLs62voWsjE',
-    mongoURI : 'mongodb://eoghan:Password1@ds343887.mlab.com:43887/email-dev',
-    cookieKey : 'Gvjkbsd78rR4wfks8fsDf98dsfG'
-};
