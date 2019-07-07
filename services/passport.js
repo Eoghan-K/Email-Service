56c24f90a87299b64c0a37cd9d731c237c2dabb9
Eoghan-K <eoghankenny95@gmail.com>
2019-07-07 21:12:41 +0100

    moving to the client side

--- services/passport.js
+++ services/passport.js
@@ -5,15 +5,15 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
-passport.serializeUser((user, done) =>{
-    done(null, user.id);
+passport.serializeUser(function (user, done) {
+    done (null, user.id);
 });
 
-passport.deserializeUser((id, done) =>{
+passport.deserializeUser(function (id, done) {
     User.findById(id)
-        .then(user => {
-            done(null, user);
-        })
+        .then (function (user) {
+            done (null, user);
+        });
 });
 
 passport.use(new GoogleStrategy({
